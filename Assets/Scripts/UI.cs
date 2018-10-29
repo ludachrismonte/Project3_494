507cd6fa0e7ed370ee139a278a5cedfa73dd58da
fboho <redcarentertainment@gmail.com>
2018-10-28 22:19:08 -0400

    UI

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -6,20 +6,30 @@ using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour {
     public Text timer;
-    public Text speedometer;
-    public Text healthText;
+    public Text [] speedometer;
+    public Text [] healthText;
     //public Button quit;
     public float allotedTime; //Length of round in seconds
     float startTime;
-    Rigidbody carRb;
-    Health health;
+    Rigidbody [] carRb;
+    Health [] health;
     string kmPerHour = "km/hr";
     //public bool changeScene = false;
 	// Use this for initialization
 	void Start () {
+        carRb = new Rigidbody[4];
+        health = new Health[4];
+
         startTime = Time.time;
-        carRb = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
-        health = GameObject.FindWithTag("Player").GetComponent<Health>();
+        carRb[0] = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
+        carRb[1] = GameObject.FindWithTag("Player2").GetComponent<Rigidbody>();
+        carRb[2] = GameObject.FindWithTag("Player3").GetComponent<Rigidbody>();
+        carRb[3] = GameObject.FindWithTag("Player4").GetComponent<Rigidbody>();
+        //health = GameObject.FindWithTag("Player").GetComponent<Health>();
+        health[0] = GameObject.FindWithTag("Player").GetComponent<Health>();
+        health[1] = GameObject.FindWithTag("Player2").GetComponent<Health>();
+        health[2] = GameObject.FindWithTag("Player3").GetComponent<Health>();
+        health[3] = GameObject.FindWithTag("Player4").GetComponent<Health>();
 	}
 	// Update is called once per frame
 	void Update () {
@@ -39,11 +49,28 @@ public class UI : MonoBehaviour {
         timer.text = minutes + ":" + seconds; 
 
         //SpeedoMeter
-        string speed = ((int)carRb.velocity.magnitude * 3.6f).ToString("f2");
-        speedometer.text = speed + " " + kmPerHour;
+        //Player1
+        string speed = ((int)carRb[0].velocity.magnitude * 3.6f).ToString("f2");
+        speedometer[0].text = speed + " " + kmPerHour;
+        //Player2
+        speed = ((int)carRb[1].velocity.magnitude * 3.6f).ToString("f2");
+        speedometer[1].text = speed + " " + kmPerHour;
+        //Player3
+        speed = ((int)carRb[2].velocity.magnitude * 3.6f).ToString("f2");
+        speedometer[2].text = speed + " " + kmPerHour;
+        //Player4
+        speed = ((int)carRb[3].velocity.magnitude * 3.6f).ToString("f2");
+        speedometer[3].text = speed + " " + kmPerHour;
 
         //Health
-        healthText.text = health.health.ToString("f2");
+        //Player1
+        healthText[0].text = health[0].health.ToString("f2");
+        //Player2
+        healthText[1].text = health[1].health.ToString("f2");
+        //Player3
+        healthText[2].text = health[2].health.ToString("f2");
+        //Player4
+        healthText[3].text = health[3].health.ToString("f2");
 
 	}
 
