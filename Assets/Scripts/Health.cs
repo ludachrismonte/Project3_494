0dcf3d2c4ff1de4310fb63c699f16ff48bac5abe
Chris Monteferrante <cpmonte@umich.edu>
2018-11-10 13:52:07 -0500

    Update Health.cs

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -37,7 +37,6 @@ public class Health : MonoBehaviour {
         //}
     }
 
-<<<<<<< HEAD
     //Currently assuming we can't skip body upgrades
     public void changeBody(int i){
         if (body < i) { 
@@ -47,8 +46,7 @@ public class Health : MonoBehaviour {
         }
     }
     //Make sure the empty game objects can't collide with the car
-=======
->>>>>>> 24d95925563071d6b11f0836c9c7bffc890f1f24
+
     private void OnCollisionEnter(Collision collision)
     {
         initialVel = carRb.velocity;
