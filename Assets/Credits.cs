ffdd51a04a84ef7cff08a2fb8095fdfcdeba851e
cpmonte <cpmonte@umich.edu>
2018-12-02 16:42:11 -0500

    asdf

---
+++ Assets/Credits.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Credits : MonoBehaviour
+{
+
+    public float speed = 120f;
+    private Rigidbody stuff;
+    private RectTransform position;
+    public RawImage Black;
+
+    // Use this for initialization
+    void Start()
+    {
+        stuff = GetComponent<Rigidbody>();
+        position = GetComponent<RectTransform>();
+        Black.color = new Color(Black.color.r, Black.color.g, Black.color.b, 1f);
+        StartCoroutine(In());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (position.transform.position.y < 700)
+        {
+            stuff.velocity = new Vector3(0f, 1f, 0f) * speed;
+        }
+        else {
+            stuff.velocity = Vector3.zero;
+            StartCoroutine(Out());
+        }
+    }
+
+    private IEnumerator In()
+    {
+        for (float i = 2f; i > 0f; i -= Time.deltaTime)
+        {
+            Black.color = new Color(Black.color.r, Black.color.g, Black.color.b, i / 2);
+            yield return null;
+        }
+        Black.color = new Color(Black.color.r, Black.color.g, Black.color.b, 0f);
+    }
+
+    private IEnumerator Out()
+    {
+        yield return new WaitForSeconds(1);
+        for (float i = 2f; i > 0f; i -= Time.deltaTime)
+        {
+            Black.color = new Color(Black.color.r, Black.color.g, Black.color.b, 1 - (i / 2));
+            yield return null;
+        }
+        Black.color = new Color(Black.color.r, Black.color.g, Black.color.b, 1f);
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/ShowLogo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ShowLogo : MonoBehaviour {
+
+    private RawImage Logo;
+
+	// Use this for initialization
+	void Start () {
+        Logo = transform.Find("Logo").GetComponent<RawImage>();
+        Logo.color = new Color(Logo.color.r, Logo.color.g, Logo.color.b, 0f);
+        StartCoroutine(Play());
+    }
+
+    private IEnumerator Play() {
+        yield return new WaitForSeconds(1);
+        for (float i = 0f; i < 1f; i += Time.deltaTime)
+        {
+            Logo.color = new Color(Logo.color.r, Logo.color.g, Logo.color.b, i);
+            yield return null;
+        }
+        yield return new WaitForSeconds(2);
+        for (float i = 1f; i > 0f; i -= Time.deltaTime)
+        {
+            Logo.color = new Color(Logo.color.r, Logo.color.g, Logo.color.b, i);
+            yield return null;
+        }
+        Logo.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Menu");
+    }
+}
