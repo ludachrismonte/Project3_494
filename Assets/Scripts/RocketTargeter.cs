4365de1ff35d393e2d72ecddbe223bcbb3739ac6
Michael Ficaro <mficaro@umich.edu>
2018-12-02 17:39:24 -0500

    Lasers

--- Assets/Scripts/RocketTargeter.cs
+++ Assets/Scripts/RocketTargeter.cs
@@ -1,22 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RocketTargeter : MonoBehaviour 
 {
     public GameObject[] m_OtherPlayers;
     public readonly float m_TargetingRange = 100f;
     public Camera m_PlayerCamera;
-    public GameObject m_Laser;
+    public Image m_Crosshairs;
 
     private WeaponManager m_WeaponManager;
     private GameObject m_CurrentTarget;
 
 	private void Start () 
     {
+        m_Crosshairs.gameObject.SetActive(false);
         m_WeaponManager = GetComponent<WeaponManager>();
         m_CurrentTarget = null;
-        m_Laser.SetActive(false);
 	}
 	
 	private void Update () 
@@ -45,9 +46,36 @@ public class RocketTargeter : MonoBehaviour
                 }
             }
             m_CurrentTarget = target;
+
+            if (m_CurrentTarget != null)
+            {
+                m_Crosshairs.gameObject.SetActive(true);
+                m_Crosshairs.transform.position = m_PlayerCamera.WorldToScreenPoint(m_CurrentTarget.transform.position);
+
+                switch(m_CurrentTarget.tag)
+                {
+                    case "Player":
+                        m_Crosshairs.color = Color.blue;
+                        break;
+                    case "Player2":
+                        m_Crosshairs.color = Color.red;
+                        break;
+                    case "Player3":
+                        m_Crosshairs.color = Color.green;
+                        break;
+                    case "Player4":
+                        m_Crosshairs.color = Color.yellow;
+                        break;
+                }
+            }
+            else
+            {
+                m_Crosshairs.gameObject.SetActive(false);
+            }
         }
         else
         {
+            m_Crosshairs.gameObject.SetActive(false);
             m_CurrentTarget = null;
         }
 	}
