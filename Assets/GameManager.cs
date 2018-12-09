9a6f83e39de74241664bd3d43feceb7b51b230bd
Michael Ficaro <mficaro@umich.edu>
2018-12-09 16:47:52 -0500

    cleanup

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject UI;
-    public GameObject GameSettings;
+    //public GameObject GameSettings;
 
     private Image explosion;
     private Image wood;
@@ -22,17 +22,12 @@ public class GameManager : MonoBehaviour
 
     private GameObject win_box;
     private Image game_black;
-    private Image menu_black;
+    //private Image menu_black;
 
     private GameObject player1;
     private GameObject player2;
     private GameObject player3;
     private GameObject player4;
-
-    private Score m_P1Score;
-    private Score m_P2Score;
-    private Score m_P3Score;
-    private Score m_P4Score;
 
 	private void Awake () 
     {
@@ -60,32 +55,31 @@ public class GameManager : MonoBehaviour
 
         win_box = UI.transform.Find("WinBox").gameObject;
         game_black = UI.transform.Find("black").GetComponent<Image>();
-        menu_black = GameSettings.transform.Find("black").GetComponent<Image>();
+        //menu_black = GameSettings.transform.Find("black").GetComponent<Image>();
 
         game_black.color = new Color(game_black.color.r, game_black.color.g, game_black.color.b, 1f);
-        menu_black.color = new Color(menu_black.color.r, menu_black.color.g, menu_black.color.b, 1f);
-        GameSettings.SetActive(true);
-        StartCoroutine(FadeIn(menu_black));
+        //menu_black.color = new Color(menu_black.color.r, menu_black.color.g, menu_black.color.b, 1f);
+        //GameSettings.SetActive(true);
+        //StartCoroutine(FadeIn(menu_black));
 
         player1 = GameObject.FindGameObjectWithTag("Player");
         player2 = GameObject.FindGameObjectWithTag("Player2");
         player3 = GameObject.FindGameObjectWithTag("Player3");
         player4 = GameObject.FindGameObjectWithTag("Player4");
 
-        m_P1Score = player1.GetComponent<Score>();
-        m_P2Score = player2.GetComponent<Score>();
-        m_P3Score = player3.GetComponent<Score>(); 
-        m_P4Score = player4.GetComponent<Score>();
-
         player1.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         player2.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         player3.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         player4.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+        // newly added
+        StartCoroutine(FadeIn(game_black));
+        StartCoroutine(GameStart());
     }
 
     public void MenuSubmit(int score, int FirehoopPoints, int FirehoopPoints_NoFlag) 
     {
-        GameSettings.SetActive(false);
+        //GameSettings.SetActive(false);
         player1.GetComponent<Score>().SetGameScore(score, FirehoopPoints, FirehoopPoints_NoFlag);
         player2.GetComponent<Score>().SetGameScore(score, FirehoopPoints, FirehoopPoints_NoFlag);
         player3.GetComponent<Score>().SetGameScore(score, FirehoopPoints, FirehoopPoints_NoFlag);
