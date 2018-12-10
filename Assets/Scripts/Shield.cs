1689769c5854b927cd78ab3d22393715fd8b8584
David Killian <david@0587380127.wireless.umich.net>
2018-12-10 01:19:25 -0500

    Fixed shield issues

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -2,36 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shield : MonoBehaviour {
+public class Shield : MonoBehaviour
+{
 
     private Vector3 initial;
     private float multiplier;
     private float timer;
     public float duration = 10f;
     public float speed = 10f;
+    private int activeShields;  // Keep track of how many shields are currently active
 
     public GameObject myCollider;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         myCollider.SetActive(false);
         timer = duration;
         initial = gameObject.transform.localScale;
         gameObject.transform.localScale = Vector3.zero;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         transform.Rotate(Vector3.up, speed * Time.deltaTime);
-        if (timer < duration) {
+        if (timer < duration)
+        {
             timer += Time.deltaTime;
             multiplier = Mathf.Sin(timer);
             multiplier = (multiplier / 12) + 1;
             gameObject.transform.localScale = initial * multiplier;
         }
-	}
+    }
 
-    public void Activate() 
+    public void Activate()
     {
         myCollider.SetActive(true);
         StartCoroutine(Run());
@@ -43,22 +48,33 @@ public class Shield : MonoBehaviour {
         gameObject.transform.localScale = Vector3.zero;
     }
 
-    private IEnumerator Run() 
+    private IEnumerator Run()
     {
-        for (float i = 0; i < 1f; i += Time.deltaTime) 
+        activeShields++;
+
+        if (activeShields == 1)
         {
-            gameObject.transform.localScale = initial * i;
-            yield return null;
+            for (float i = 0; i < 1f; i += Time.deltaTime)
+            {
+                gameObject.transform.localScale = initial * i;
+                yield return null;
+            }
         }
+
         timer = 0f;
         yield return new WaitForSeconds(duration);
-        myCollider.SetActive(false);
-        Vector3 current = gameObject.transform.localScale;
-        for (float i = 1; i > 0f; i -= Time.deltaTime)
+
+        if (activeShields == 1)
         {
-            gameObject.transform.localScale = current * i;
-            yield return null;
+            myCollider.SetActive(false);
+            Vector3 current = gameObject.transform.localScale;
+            for (float i = 1; i > 0f; i -= Time.deltaTime)
+            {
+                gameObject.transform.localScale = current * i;
+                yield return null;
+            }
         }
+        activeShields--;
     }
 
 }
