1743e9c55daa6650e9f6b3d08cc5f24f51ce1e2f
Michael Ficaro <mficaro@umich.edu>
2018-12-10 00:28:51 -0500

    fixes

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -54,8 +54,13 @@ public class ControllerInput : MonoBehaviour
         float horizInput = player.LeftStick.Vector.x;
         float vertInput = player.LeftStick.Vector.y;
         float gasInput = player.Action1.Value; // A button
-        float brakeInput = player.Action3.Value; // X button
 
+        // Braking/reverse (X button)
+        if (gasInput <= 0.0f)
+        {
+            gasInput = -player.Action3.Value;
+        }
+        
         if (player.Action4.WasPressed && GetComponent<RespawnReset>().stuck && transform.position.y >= 2.5)
         {
             GetComponent<RespawnReset>().ResetCar();
@@ -74,7 +79,7 @@ public class ControllerInput : MonoBehaviour
             }
         }
 
-        car.Move(horizInput, gasInput, brakeInput, 0);
+        car.Move(horizInput, gasInput, 0, 0);
 
         // Weapons
         if (player.Action2.WasPressed) 
